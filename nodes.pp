f8ea90029bf8c101f22c6cda49e728b3fb68099b
Geo P.C <geopc@citrusinformatics.com>
2011-09-12 14:44:37 +0530

    :)

--- nodes.pp
+++ nodes.pp
@@ -1,10 +1,3 @@
-$check_external_commands=yes
-$link = "yes"
-$monitor = "yes"
-$firewall = "yes"
-$my_project_onmodule = "no"
-$plugins = "yes"
-$use_ssl = "no"
 
 Exec { 
     path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin"], 
