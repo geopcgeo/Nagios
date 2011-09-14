02b46e1acb82d4688754fa2e4c69e72213faab8f
Geo P.C <geopc@citrusinformatics.com>
2011-09-14 08:48:16 +0530

    :)

--- nagios/scripts/ubuntu_web_interface_password.sh
+++ nagios/scripts/ubuntu_web_interface_password.sh
@@ -6,4 +6,4 @@ htpasswd -b -c /etc/nagios3/htpasswd.users nagiosadmin $nagiosadmin_password
 sudo /etc/init.d/nagios3 restart
 sudo /etc/init.d/nagios-nrpe-server restart
 sudo /etc/init.d/apache2 restart
-echo "Now you can login to Nagios Web Interface with url http://<IP Address>/nagios3/ with username nagiosadmin.
+echo "Now you can login to Nagios Web Interface with url http://<IP Address>/nagios3/ with username nagiosadmin."
